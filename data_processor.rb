25540a1eedb0aeedd6724d26b5e88a067d7b20e4
doridoridoriand <i.am.eager.for.peace@gmail.com>
2015-05-19 14:42:55 +0900

    [WIP] create view

--- data_processor.rb
+++ data_processor.rb
@@ -1,8 +1,14 @@
 $: << File.expand_path(File.join(__FILE__, '..'))
 require 'base'
 require 'google_trend_parser'
+require 'fashion_press_parser'
 
 GoogleTrendParser::DATA.each do |entry|
+  puts entry
+  puts '------'
+end
+
+FashionPressParser::DATA.each do |entry|
   puts entry
   puts '------'
 end

c86bc0a38c599b6e2966c868620617a1515d01bb
doridoridoriand <i.am.eager.for.peace@gmail.com>
2015-05-27 16:04:46 +0900

    fix code

--- data_processor.rb
+++ data_processor.rb
@@ -1,7 +1,6 @@
 $: << File.expand_path(File.join(__FILE__, '..', 'parsers'))
 require 'base'
 require 'google_trend_parser'
-#require 'google_global_trend_parser'
 require 'fashion_press_parser'
 
 #module DataProcessor
@@ -11,15 +10,7 @@ require 'fashion_press_parser'
 #  }
 #end
 
-GoogleTrendParser::DATA.each do |entry|
-  entry.map {|entry| puts entry}
-  puts '------'
-end
-
-#GoogleGlobalTrendParser::DATA.each do |entry|
-#  entry.map {|entry| puts entry}
-#  puts '------'
-#end
+puts GoogleTrendParser::DATA
 
 #FashionPressParser::DATA.each do |entry|
 #  entry.map {|entry| puts entry}
--- parsers/google_trend_parser.rb
+++ parsers/google_trend_parser.rb
@@ -1,13 +1,13 @@
 $: << File.expand_path(File.join(__FILE__, '..'))
 require 'base'
-require 'open-uri'
 
-data = ''
-open("https://www.google.co.jp/trends/explore#cat=0-18-68&geo=JP&cmpt=q&tz=") {|filestream|
-  filestream.each_line {|line|
-    data << line.encode("UTF-8")
+module GoogleTrendParser
+  data = ''
+  open("https://www.google.co.jp/trends/explore#cat=0-18-68&geo=JP&cmpt=q&tz=") {|filestream|
+    filestream.each_line {|line|
+      data << line.encode("UTF-8")
+    }
   }
-}
-
-puts data.split('trends.Category.setTreeData')[1].split('</script>')[0]
-
+  json_rough = data.split('trends.Category.setTreeData(')[1].split('</script>')[0]
+  DATA = json_rough.rstrip!.chop!.chop!
+end

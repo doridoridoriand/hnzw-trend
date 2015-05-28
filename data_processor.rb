6e1fd6acf8e43fda0fe1190e8d74f6aadf940962
doridoridoriand <i.am.eager.for.peace@gmail.com>
2015-05-28 14:13:02 +0900

    add function to generate hash

--- data_processor.rb
+++ data_processor.rb
@@ -2,19 +2,14 @@ $: << File.expand_path(File.join(__FILE__, '..', 'parsers'))
 require 'base'
 require 'google_trend_parser'
 require 'fashion_press_parser'
+require 'wear_parser'
 
-#module DataProcessor
-#  {
-#    title: Forgery(:name).full_name,
-#    text: Forgery(:internet).email_addres
-#  }
-#end
 
-json_data = JSON.parse(GoogleTrendParser::DATA.encode('UTF-8'))
-puts json_data["children"]
+
+#json_data = JSON.parse(GoogleTrendParser::DATA.encode('UTF-8'))
+#puts json_data["children"]
 
 #FashionPressParser::DATA.each do |entry|
-#:w
 #entry.map {|entry| puts entry}
 #  puts '------'
 #end
--- parsers/wear_parser.rb
+++ parsers/wear_parser.rb
@@ -1,26 +1,24 @@
 $: << File.expand_path(File.join(__FILE__, '..'))
 require 'base'
-require 'pp'
 
-headless = Headless.new
-headless.start
-driver = Selenium::WebDriver.for :chrome
+module WearParser
+  headless = Headless.new
+  headless.start
+  driver = Selenium::WebDriver.for :chrome
 
-result_arr = []
+  Base::WEAR_URL_END_ARR.each do |url_end|
+    driver.navigate.to "http://wear.jp/sp/keyword/#{url_end.to_s}/"
+    data = driver.find_element(:id, "keyword_list").find_elements(:xpath, "./ul/li")
+    hash = Hash.new {|h, e| h[e] = {}}
+    data.each_with_index do |entry, i|
+      puts entry.text
+      hash[i][:type] = url_end.to_s
+      hash[i][:text] = entry.text
+    end
+  end
 
-Base::WEAR_URL_END_ARR.each do |url_end|
-  driver.navigate.to "http://wear.jp/sp/keyword/#{url_end.to_s}/"
-  puts "http://wear.jp/sp/keyword/#{Base::WEAR_URL_END_ARR[0]}/"
-  puts driver.title
-  data = driver.find_element(:id, "keyword_list").find_elements(:xpath, "./ul/li")
-  data.map {|node| result_arr << node.text}
+  driver.quit
+  headless.destroy
+
+  DATA = hash
 end
-
-driver.quit
-headless.destroy
-
-hash = Hash.new {|h, e| h[e] = {}}
-result_arr.each_with_index do |entry, i|
-  hash[i][:text] = entry
-end
-DATA = hash

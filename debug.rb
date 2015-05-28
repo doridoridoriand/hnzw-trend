6cc33c258dc8a54cd6f307d1f368e0b5e195b823
doridoridoriand <i.am.eager.for.peace@gmail.com>
2015-05-28 13:45:44 +0900

    remove needless files

--- debug.rb
+++
@@ -1,9 +0,0 @@
-$: << File.expand_path(File.join(__FILE__, '..', 'parsers'))
-require 'base'
-require 'fashion_press_parser'
-
-#puts $: << File.expand_path(File.join(__FILE__, '..', 'parsers'))
-
-FashionPressParser::DATA.each do |entry|
-  puts entry
-end

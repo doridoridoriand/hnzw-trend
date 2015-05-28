7178ae31ee6a2329ef2ab4e5f590ecdf96a9015b
doridoridoriand <i.am.eager.for.peace@gmail.com>
2015-05-28 13:31:51 +0900

    created wear parser

--- parsers/wear_parser.rb
+++ parsers/wear_parser.rb
@@ -6,14 +6,21 @@ headless = Headless.new
 headless.start
 driver = Selenium::WebDriver.for :chrome
 
+result_arr = []
+
 Base::WEAR_URL_END_ARR.each do |url_end|
-  #driver.navigate.to "http://wear.jp/sp/keyword/women/"
   driver.navigate.to "http://wear.jp/sp/keyword/#{url_end.to_s}/"
-  puts driver.title
   puts "http://wear.jp/sp/keyword/#{Base::WEAR_URL_END_ARR[0]}/"
+  puts driver.title
   data = driver.find_element(:id, "keyword_list").find_elements(:xpath, "./ul/li")
-  data.map {|node| puts node.text}
+  data.map {|node| result_arr << node.text}
 end
 
 driver.quit
 headless.destroy
+
+hash = Hash.new {|h, e| h[e] = {}}
+result_arr.each_with_index do |entry, i|
+  hash[i][:text] = entry
+end
+DATA = hash
